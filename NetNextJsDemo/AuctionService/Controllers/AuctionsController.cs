ee81929c209b55e6378853bdf9a9bc8b04c0d05b
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2024-06-29 15:41:51 +0300

    Update AuctionsController.cs

--- NetNextJsDemo/AuctionService/Controllers/AuctionsController.cs
+++ NetNextJsDemo/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,7 +15,7 @@ namespace AuctionService.Controllers
         private readonly AuctionDbContext _auctionDbContext;
         private readonly IMapper _mapper;
 
-        public AuctionsController(AuctionDbContext auctionDbContext,IMapper mapper)
+        public AuctionsController(AuctionDbContext auctionDbContext, IMapper mapper)
         {
             _auctionDbContext = auctionDbContext;
             _mapper = mapper;
@@ -24,8 +25,8 @@ namespace AuctionService.Controllers
         public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
         {
             var auctions = await _auctionDbContext.Auctions
-                .Include(x=>x.Item)
-                .OrderBy(x=>x.Item.Make)
+                .Include(x => x.Item)
+                .OrderBy(x => x.Item.Make)
                 .ToListAsync();
 
             return _mapper.Map<List<AuctionDto>>(auctions);
@@ -37,11 +38,29 @@ namespace AuctionService.Controllers
             var auction = await _auctionDbContext.Auctions
                 .Include(x => x.Item)
                 .FirstOrDefaultAsync(x => x.Id == id);
-            
-            if (auction == null) 
+
+            if (auction == null)
                 return NotFound();
 
             return _mapper.Map<AuctionDto>(auction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto createAuctionDto)
+        {
+            var auction = _mapper.Map<Auction>(createAuctionDto);
+            //TODO: add curent user as a seller
+
+            auction.Seller = "testSeller";
+            _auctionDbContext.Auctions.Add(auction);
+
+            var result = await _auctionDbContext.SaveChangesAsync() > 0;
+
+            if (result)
+                return BadRequest("Couldnt save changes to database");
+
+            return CreatedAtAction(nameof(GetAuctionById), 
+                new { auction.Id }, _mapper.Map<AuctionDto>(auction));
         }
     }
 }
